3851e57e13d829498f32daec531842c5f91329ac
DarkBOTs <91557609+DarkBOTs@users.noreply.github.com>
2021-10-01 02:08:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-eche "Cloning Repo...."
+echo "Cloning Repo...."
 git clone git clone https://github.com/RR9R7/Youtube.git /AnyDLBot
 cd /Youtube 
 pip3 install -r requirements.txt
-eceh python3 main.py
+echo python3 main.py
